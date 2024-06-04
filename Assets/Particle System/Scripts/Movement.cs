93a06811f58821af3b67f440c286ca0bfeb8f6cc
Demine Nguyen <88977236+Demine2309@users.noreply.github.com>
2024-06-04 20:12:14 +0700

    small update
    4/6/2024

--- Assets/Particle System/Scripts/Movement.cs
+++ Assets/Particle System/Scripts/Movement.cs
@@ -4,7 +4,42 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-        
+    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float dragForce;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        PlayerRotatation();
+
+        MoveForward();
+    }
+
+    private void PlayerRotatation()
+    {
+        float rotationSpeed = 90;
+
+        if (Input.GetKey(KeyCode.A))
+            rb.transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            rb.transform.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
+    }
+
+    private void MoveForward()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector2 force = transform.up * moveSpeed;
+
+            rb.AddForce(force);
+
+            rb.drag = dragForce;
+        }
+    }
 }
